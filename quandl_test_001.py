9288fdb4edc7d57a8bc6ef35f269b1adbf6474e0
Hemant Satam <hsatam@users.noreply.github.com>
2020-02-16 00:39:37 +0530

    Update quandl_test_001.py
    Created a 30 day projection using LinearRegression Model

--- quandl_test_001.py
+++ quandl_test_001.py
@@ -1,9 +1,14 @@
 # Import Libraries
 import quandl
-import numpy as numpy
+import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 
 def getQuandlInfo(stock, st_date, ed_date, returnType):
 	#Data download 
@@ -21,15 +26,35 @@ START_DATE = "2019-01-01"
 END_DATE = "2019-12-31"
 RETURNS = "pandas"
 
+#Number of days to prdict price
+forecast_time = int(30)
+
 data = getQuandlInfo(STOCK, START_DATE, END_DATE, RETURNS)
 
 #print info of data returned
-print (data.head(10),"\n\n")
-print (data.info())
+print (data.tail(10),"\n\n")
+#print (data.info())
 
 #Plotting data received
-plt.style.use("classic")
-data["Close"].plot(label="TATA Motors", figsize=(16,8), title="Adjusted Closing Price")
-plt.legend()
-plt.show()
+#plt.style.use("classic")
+#data["Close"].plot(label="TATA Motors", figsize=(16,8), title="Adjusted Closing Price")
+#plt.legend()
+#plt.show()
 
+data["prediction"] = data["Close"].shift(-1)
+data.dropna(inplace=True)
+
+X = np.array(data.drop(["prediction"], 1))
+Y = np.array(data["prediction"])
+
+scale = StandardScaler()
+X = scale.fit_transform(X)
+X_prediction = X[-forecast_time:]
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.5)
+
+clf = LinearRegression()
+clf.fit(X_train, Y_train)
+prediction = (clf.predict(X_prediction))
+
+print (prediction)
