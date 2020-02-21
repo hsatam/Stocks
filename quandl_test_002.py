e552ead36c939a4de7ef2a254a413bc4bbdded2b
Hemant Satam <16187260+hsatam@users.noreply.github.com>
2020-02-21 19:32:52 +0530

    update y_pred logic
    Updated y_pred  logic to match columns to update

--- quandl_test_002.py
+++ quandl_test_002.py
@@ -8,7 +8,7 @@ np.set_printoptions(suppress=True)
 
 # Initialize Stocks class for operations
 stock = Stocks()
-model = ['LR']
+model = ['OLS']
 
 # Retrieve data from Quandl using Stocks class
 data = stock.getStockDetails(Stocks.STOCK, Stocks.START_DATE, Stocks.END_DATE, Stocks.RETURNS)
@@ -27,13 +27,11 @@ for mdl in model:
 		# Build a OLS Regression Model and carryout predictions
 		pred_col, model = stock.buildOLSModel(X_train, X_test, Y_train, Y_test)
 
-
 	y_pred_ctr = 0
 
 	for ctr in range((len(data.index) - len(pred_col)), len(data.index)):
-		data.iat[ctr,8] = "{:.2f}".format(pred_col[y_pred_ctr])
+		data.iat[ctr,(len(data.columns) - 1)] = "{:.2f}".format(pred_col[y_pred_ctr])
 		y_pred_ctr += 1
-
 
 	stock.printModelMetrics (model, X_train, Y_train, Y_test, pred_col)
 
